ecac8057a6a9ba909c6829552098ce30727fce98
missmurs <131698859+missmurs@users.noreply.github.com>
2023-12-03 01:01:07 +0200

    ALL CODE

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
+document.addEventListener("DOMContentLoaded", function () {
+  const gallery = document.querySelector(".gallery");
+  const modal = basicLightbox.create(document.getElementById("modal"));
+  gallery.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (e.target.classList.contains("gallery__image")) {
+      const imageUrl = e.target.dataset.source;
+      document.getElementById("modal-image").src = imageUrl;
+      modal.show();
+    }
+  });
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && modal.visible()) {
+      modal.close();
+    }
+  });
+});
