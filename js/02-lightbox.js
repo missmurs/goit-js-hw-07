5d23e4d0b4442f18871680e184e4703288c16069
missmurs <131698859+missmurs@users.noreply.github.com>
2023-12-03 01:25:54 +0200

    Update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,52 +1,38 @@
 import { galleryItems } from "./gallery-items.js";
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
-
 const container = document.querySelector(".gallery");
 container.insertAdjacentHTML("beforeend", galleryList(galleryItems));
 container.addEventListener("click", handleClick);
-
 function galleryList(arr) {
   return arr
     .map(
       ({ preview, original, description }) =>
         `
-      <li class="gallery__item">
-        <a class="gallery__link" href="${original}">
-          <img class="gallery__image" src="${preview}" alt="${description}">
-        </a>
-      </li>
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}">
+    </a>
+    </li>
     `
     )
     .join("");
 }
-
 function handleClick(event) {
   event.preventDefault();
 
-  if (event.target === event.currentTarget) {
-    return;
+  if (event.target === event.curruntTarget) {
+    return gallery.next();
   }
 
-  const gallery = new SimpleLightbox(".gallery__item a", {
+  let gallery = new SimpleLightbox(".gallery__item a", {
     captionsData: "alt",
     captionDelay: 250,
     overlayOpacity: 0.5,
   });
-
-  gallery.on("show.simplelightbox", function (e) {
-    const currentItemIndex = galleryItems.findIndex(
-      (item) => item.original === e._currentTarget.href
-    );
-
-    // Заміна вмісту модального вікна на обране зображення
-    const modalImage = `
-      <img class="gallery__image" src="${galleryItems[currentItemIndex].original}" alt="${galleryItems[currentItemIndex].description}">
-    `;
-
-    // Вставка HTML в модальне вікно
-    gallery.setContent(modalImage);
+  gallery.on("show.simplelightbox", function () {
+    `
+        <img class="gallery__image " src="${galleryItems.original}" alt="${galleryItems.description}">
+        `;
   });
 }
