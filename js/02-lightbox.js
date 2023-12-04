639a15c9e0559f8d5ea6c6c8ae2375274535b776
missmurs <131698859+missmurs@users.noreply.github.com>
2023-12-04 10:54:23 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,7 +3,7 @@ import { galleryItems } from "./gallery-items.js";
 console.log(galleryItems);
 const container = document.querySelector(".gallery");
 container.insertAdjacentHTML("beforeend", galleryList(galleryItems));
-container.addEventListener("click", handleClick);
+
 function galleryList(arr) {
   return arr
     .map(
@@ -18,21 +18,9 @@ function galleryList(arr) {
     )
     .join("");
 }
-function handleClick(event) {
-  event.preventDefault();
 
-  if (event.target === event.curruntTarget) {
-    return gallery.next();
-  }
-
-  let gallery = new SimpleLightbox(".gallery__item a", {
-    captionsData: "alt",
-    captionDelay: 250,
-    overlayOpacity: 0.5,
-  });
-  gallery.on("show.simplelightbox", function () {
-    `
-        <img class="gallery__image " src="${galleryItems.original}" alt="${galleryItems.description}">
-        `;
-  });
-}
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  overlayOpacity: 0.5,
+});
